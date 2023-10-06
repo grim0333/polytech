0916097f69aca7f68cd092c3538f123c2bcd580f
KOPO <KOPO@DESKTOP-K5VK0JI>
2023-10-06 15:31:43 +0900

    보완 프로젝트 - 계정 기능 변경 초안

--- kopoResort/src/main/java/kr/ac/kopo/ctc/resort/dao/AccountDAO.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.kopo.ctc.resort.dao;
-
-import kr.ac.kopo.ctc.resort.domain.AccountDomain;
-
-public interface AccountDAO {
-	public AccountDomain AccCheck (String id , String passwd);
-	
-}
